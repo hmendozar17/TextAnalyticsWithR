677e06b351a48448d3b142793d9849b8cdb1eea9
Gloria Bautista <glorobame@Glorias-iMac.local>
2019-10-15 16:20:58 -0500

    normality

--- first_script.R
+++ first_script.R
@@ -1,5 +1,24 @@
 #Henry Text Analytics with R
+# https://github.com/datasciencedojo/IntroToTextAnalyticsWithR
 #Reference: https://www.youtube.com/watch?v=4vuw0AsHeGw
+# rehttps://github.com/datasciencedojo/IntroToTextAnalyticsWithR
+
 a=1
 b=2
 c=1
+
+x<-runif(10, min=1, max=3) 
+hist(x)
+install.packages("e1071")
+library(e1071)
+kurtosis(x)  
+xn<-rnorm(10000000,3,2)
+hist(xn)
+kurtosis(xn)
+qqplot(xn)
+xnsample<-rnorm(200,3,2)
+qqplot(xn,xnsample)
+qqplot
+qqnorm(xnsample)
+install.packages("ExtDist")
+xl<-rLaplace(1000,3,2)
